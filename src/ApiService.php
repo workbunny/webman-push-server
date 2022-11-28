176ed0d9dd99f90c0b9ad8896e8e10d1fa74ef9f
chaz6chez <250220719@qq.com>
2022-11-28 18:43:30 +0800

    fixed bugs

--- src/ApiService.php
+++ src/ApiService.php
@@ -36,13 +36,13 @@ class ApiService implements ServerInterface
             $connection->send(new Response(404, [], 'Not Found'));
         }
 
-        $result = array_reduce(
+        $result = call_user_func(array_reduce(
             array_reverse(ApiRoute::getMiddlewares(ApiRoute::getMiddlewareTag($res[1]), $data->method())),
             function (Closure $next, Closure $handler) use ($data, $res) {
                 return $handler($next, $this, $data, $res[2]);
             },
             $res[1]
-        );
+        ));
 
         if(!$result instanceof Response){
             $connection->send(new Response(500, [], 'Server Error'));
--- src/config/plugin/workbunny/webman-push-server/process.php
+++ src/config/plugin/workbunny/webman-push-server/process.php
@@ -9,13 +9,13 @@ return [
     // 主服务
     'push-server' => [
         'handler'     => Server::class,
-        'listen'      => config('plugin.workbunny.webman-push-server.app.ws_host'),
+        'workbunny'      => config('plugin.workbunny.webman-push-server.app.push-server.ws_host'),
         'count'       => config('plugin.workbunny.webman-push-server.app.debug') ? 1 : cpu_count() * 2,
         'reloadable'  => false, // 执行reload不重启
         'services'    => [
             ApiService::class => [
                 'handler'     => ApiService::class,
-                'listen'      => config('plugin.workbunny.webman-push-server.app.push_server.api_host'),
+                'listen'      => config('plugin.workbunny.webman-push-server.app.push-server.api_host'),
             ]
         ]
     ],
