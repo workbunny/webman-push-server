4db367ea4c98c9e49ded8059e151d661afcfe455
chaz6chez <250220719@qq.com>
2022-11-29 16:30:11 +0800

    updated: 1. 调整结构，增加测试支持

--- tests/ApiServiceTest.php
+++ tests/ApiServiceTest.php
@@ -30,37 +30,4 @@ class ApiServiceTest extends TestCase
         ApiRoute::start($this->worker);
         parent::setUp();
     }
-
-    public function testAuthLogin()
-    {
-        $this->client()::$mockHandler = new MockHandler([new Response()]);
-        $this->client()->auth->login('test', '123123');
-
-        $request = $this->client()::$mockHandler->getLastRequest();
-
-        $this->assertEquals(
-            '/nacos/v1/auth/users/login',
-            $request->getUri()->getPath()
-        );
-        $this->assertEquals(
-            '127.0.0.1',
-            $request->getUri()->getHost()
-        );
-        $this->assertEquals(
-            8848,
-            $request->getUri()->getPort()
-        );
-        $this->assertEquals(
-            'username=test',
-            $request->getUri()->getQuery()
-        );
-        $this->assertEquals(
-            'password=123123',
-            $request->getBody()->getContents()
-        );
-        $this->assertEquals(
-            'POST',
-            $request->getMethod()
-        );
-    }
 }
