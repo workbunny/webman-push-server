e7058fdf48fa6d1c78b9f3a9c22d47bb02f37de6
chaz6chez <250220719@qq.com>
2022-12-16 14:18:08 +0800

    fixed bugs

--- src/Server.php
+++ src/Server.php
@@ -33,7 +33,7 @@ class Server implements ServerInterface
     /**
      * @var string $version version
      */
-    public static string $version = '0.1.10';
+    public static string $version = '0.1.11';
 
     /**
      * @var bool $debug debug mode
