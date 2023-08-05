849dedafed03976b117197b1e0ec2d13df222bc6
chaz6chez <chaz6chez1993@outlook.com>
2023-08-05 17:37:54 +0800

    update 1. 增加requeue机制，进一步保障队列数据不会丢失 2. 调整配置

---
+++ src/config/plugin/workbunny/webman-push-server/database.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+return [
+    'connections' => [
+        'push-server' => [
+            'driver'   => 'sqlite',
+            'database' => '',
+            'prefix'   => '',
+        ],
+    ]
+];
