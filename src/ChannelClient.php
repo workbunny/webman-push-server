69d81882f947da2b1e2d5289008cf0ad19657efc
chaz6chez <chaz6chez1993@outlook.com>
2024-04-23 11:08:34 +0800

    使用redis pub/sub代替channel

--- src/ChannelClient.php
+++ src/ChannelClient.php
@@ -3,9 +3,11 @@
 namespace Workbunny\WebmanPushServer;
 
 use Exception;
+use Workerman\Redis\Client;
 
 class ChannelClient extends \Channel\Client
 {
+    protected static ?Client $_redisClient = null;
     /**
      *
      * @return bool
@@ -16,43 +18,62 @@ class ChannelClient extends \Channel\Client
     }
 
     /**
-     * @param $ip
-     * @param $port
+     * @param string $ip
+     * @param int $port
+     * @param array $options
      * @return void
      * @throws Exception
      */
-    public static function connect($ip = '127.0.0.1', $port = 2206): void
+    public static function connect($ip = '127.0.0.1', $port = 2206, array $options = []): void
     {
         if (self::isChannelEnv()) {
             parent::connect($ip, $port);
-        } else {
 
+        } else {
+            self::$_redisClient = (new Client("redis://$ip:$port", $options));
+            self::$_redisClient?->connect();
         }
     }
 
+    /**
+     * @param string $event
+     * @param callable $callback
+     * @return void
+     * @throws Exception
+     */
     public static function on($event, $callback): void
     {
         if (self::isChannelEnv()) {
             parent::on($event, $callback);
         } else {
-            // todo redis pub sub
+            self::$_redisClient?->subscribe("workbunny:webman-push-server:$event", function ($channel, $message) use ($callback) {
+                call_user_func($callback, unserialize($message));
+            });
         }
     }
 
     /**
-     * @param $events
+     * @param string $events
      * @param $data
      * @param bool $is_loop
      * @return bool|null
      * @throws Exception
      */
-    public static function publish($events, $data , $is_loop = false): ?bool
+    public static function publish($events, $data, $is_loop = false): ?bool
     {
+        $events = (array)$events;
         $type = $is_loop ? 'publishLoop' : 'publish';
-        return self::sendAnyway(array('type' => $type, 'channels' => (array)$events, 'data' => $data));
+        if (self::isChannelEnv()) {
+            return self::sendAnyway(array('type' => $type, 'channels' => $events, 'data' => $data));
+        }
+        foreach ($events as $event) {
+            self::$_redisClient?->publish("workbunny:webman-push-server:$event", serialize($data));
+        }
+        return true;
     }
 
     /**
+     * @inheritDoc
      * @param $data
      * @return bool|null
      * @throws Exception
@@ -62,10 +83,7 @@ class ChannelClient extends \Channel\Client
         self::connect(self::$_remoteIp, self::$_remotePort);
         $body = serialize($data);
         if (self::$_isWorkermanEnv) {
-            if (self::isChannelEnv()) {
-                return self::$_remoteConnection->send($body);
-            }
-            // todo redis pub sub
+            return self::$_remoteConnection->send($body);
         } else {
             throw new Exception('Not workerman env. ');
         }
