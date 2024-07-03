d26434ba0c1fc128a326f2eb9bab4bb0f2de22d6
chaz6chez <chaz6chez1993@outlook.com>
2024-07-03 11:45:40 +0800

    update

--- src/config/plugin/workbunny/webman-push-server/redis.php
+++ src/config/plugin/workbunny/webman-push-server/redis.php
@@ -3,14 +3,14 @@
 return [
     // push server 储存器
     'server-storage' => [
-        'host'     => '172.17.0.1',
+        'host'     => 'redis',
         'password' => '',
         'port'     => 6379,
         'database' => 0,
     ],
     // 服务通讯频道
     'server-channel' => [
-        'host'     => '172.17.0.1',
+        'host'     => 'redis',
         'password' => '',
         'port'     => 6379,
         'database' => 0,
